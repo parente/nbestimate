794ead93d5abda462b4fa9b9a4c8696879196286
Peter Parente <parente@cs.unc.edu>
2017-03-11 22:30:58 -0500

    Remove commas from count input

--- update.py
+++ update.py
@@ -6,13 +6,15 @@ from datetime import datetime
 def main():
     dt = datetime.now().strftime('%Y-%m-%d')
     try:
-        count = sys.argv[1].strip()
+        count = sys.argv[1]
     except IndexError:
         count = input('Count for {}: '.format(dt))
+    count = count.strip().replace(',', '')
     line = '{},{}\n'.format(dt, int(count))
     with open('ipynb_counts.csv', 'a') as f:
         f.write(line)
     print('Done')
+
 
 if __name__ == '__main__':
     main()
